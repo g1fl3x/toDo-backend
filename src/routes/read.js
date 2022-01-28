a2ee5905a1ffa9d94b0c611dd26bf9ed00484801
g1fl3x <g1fl3x>
2022-01-28 16:54:50 +0300

    set perpage range

--- src/routes/read.js
+++ src/routes/read.js
@@ -1,21 +1,21 @@
 const { getTasks } = require('../db')
 
 module.exports = (req, res) => {
-    if (!/\d+$/.test(req.params.userId)) 
-        return res.status(404).json({message: "userId is not int"})
-    if (req.query.page < 1)
-        req.query.page = 1
+    if (!/\d+$/.test(req.params.userId))
+        return res.status(404).json({ message: "userId is not int" })
+    if (req.query.page < 1 || req.query.page > 20)
+        return res.status(400).json({ message: "perpage param must be in range [1...20]" })
 
     const args = [
         req.params.userId,
         req.query.filterBy ?? 'all',
         req.query.order ?? 'asc',
-        req.query.pp ?? 1,
+        req.query.pp,
         req.query.page ?? 1
     ]
     try {
         return res.json(getTasks(...args))
-    } catch(e) {
-        return res.status(400).json({message: e})
+    } catch (e) {
+        return res.status(400).json({ message: e })
     }
 };
