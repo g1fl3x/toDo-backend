dbbe50f582395bac70438998a7f2356ca0043c85
g1fl3x <g1fl3x>
2022-01-28 15:41:33 +0300

    add db logic

--- src/db.js
+++ src/db.js
@@ -1,3 +1,44 @@
+const { v4 } = require('uuid')
+const fs = require('fs')
+const { dbFile } = require('./config')
+
 module.exports = {
-    
+    getTasks: (userId, filterBy, order, pp, page) => {
+        // 1, 'all', 'asc', 5, 1
+
+        const usersList = JSON.parse(fs.readFileSync(dbFile, 'utf-8'))
+        const tasksList = usersList[String(userId)]
+        const filteredTasks = tasksList.filter(task => {
+            if (filterBy === 'done') return task.done ? true : false
+            if (filterBy === 'undone') return task.done ? false : true
+            return true
+        })
+        const count = filteredTasks.length
+
+        filteredTasks.sort((a, b) => order === 'desc' ? b.createdAt - a.createdAt : a.createdAt - b.createdAt)
+        const outputTasks = filteredTasks.slice((page - 1) * pp, page * pp)
+        return { count: count, tasks: [...outputTasks] }
+
+    },
+    addTask: (userId, name, done) => {
+        const task = {
+            uuid: v4(),
+            name: name,
+            done: done,
+            createdAt: +new Date(),
+        }
+        const usersList = JSON.parse(fs.readFileSync(dbFile, 'utf-8'))
+        usersList[String(userId)].push(task)
+        fs.writeFileSync(dbFile, JSON.stringify(usersList))
+    },
+    deleteTask: (userId, taskId) => {
+        const usersList = JSON.parse(fs.readFileSync(dbFile, 'utf-8'))
+        usersList[String(userId)] = usersList[String(userId)].filter(task => taskId === task.uuid ? false : true)
+        fs.writeFileSync(dbFile, JSON.stringify(usersList))
+    },
+    updateTask: (userId, taskId, newTask) => {
+        const usersList = JSON.parse(fs.readFileSync(dbFile, 'utf-8'))
+        usersList[String(userId)] = usersList[String(userId)].map(task => taskId === task.uuid ? { ...task, ...newTask } : task)
+        fs.writeFileSync(dbFile, JSON.stringify(usersList))
+    },
 }
