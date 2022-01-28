8bf56e011660feb544888dd705e0aac2a7b5b99e
g1fl3x <g1fl3x>
2022-01-28 15:42:00 +0300

    add cors and json middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = require('./router')
+const cors = require('cors')
 const { port } = require('./config')
 
 const app = express()
 
+app.use(cors());
+app.options('*', cors());
+app.use(express.json())
 app.use(router)
 
 app.listen(port, () => {
