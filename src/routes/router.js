03b73693a91451dd4df3dda22deb793b4fb97b71
g1fl3x <g1fl3x>
2022-02-01 12:34:41 +0300

    add validator middleware

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+router.use('/', require('./validator')) // validate data
+
 router.use('/', require('./read'))
 router.use('/', require('./update'))
 router.use('/', require('./create'))
