a174cd4f7a90802c05dfac58bfb3375c71739e62
g1fl3x <g1fl3x>
2022-02-02 13:40:34 +0300

    add dotenv in config file

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
     apiUri: '/api',
+    dbType: 'postgres',
     port: process.env.PORT || 3000,
     username: process.env.USERNAME || '',
     password: process.env.PASSWORD || '',
     dbName: process.env.DBNAME || '',
-    dbType: process.env.DBTYPE || 'postgres',
     host: process.env.HOST || 'localhost'
 }
