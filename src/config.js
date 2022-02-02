941af2fb93917a82e44d2bd995749c48c24aac65
g1fl3x <g1fl3x>
2022-02-02 12:32:41 +0300

    add new config fields

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 module.exports = {
     apiUri: '/api',
     port: process.env.PORT || 3000,
-    dbFile: 'database.sqlite'
+    username: process.env.USERNAME || '',
+    password: process.env.PASSWORD || '',
+    dbName: process.env.DBNAME || '',
+    dbType: process.env.DBTYPE || 'postgres',
+    host: process.env.HOST || 'localhost'
 }
