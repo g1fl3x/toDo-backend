4e098876b93cc12d1b54ba8302cfb18c285c5549
g1fl3x <g1fl3x>
2022-02-03 13:29:01 +0300

    change task.delete  method for delete

--- src/task/task.delete.js
+++ src/task/task.delete.js
@@ -1,4 +1,4 @@
-const { Task } = require('../db')
+const { Task } = require('../models/index')
 const { errorsCheck } = require('../utils')
 const { param, validationResult } = require('express-validator')
 
@@ -11,13 +11,12 @@ router.delete('/task/:userId/:taskId',
 
     async (req, res) => {
         try {
-            const result = await Task.findOne({
+            await Task.destroy({
                 where: {
                     uuid: req.params.taskId,
-                },
+                }
             })
-            await result.destroy()
-            return res.json(result)
+            return res.json({ message: "ok" })
         } catch (err) {
             return res.status(500).json({ message: String(err) })
         }
