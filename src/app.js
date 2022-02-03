0b222374f24ace7a2c16be13985afd0999ddcb59
g1fl3x <g1fl3x>
2022-02-03 13:28:33 +0300

    use recursive collector

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = require('./task/router')
 const cors = require('cors')
-const { port, apiUri } = require('./config')
-
+const { port, apiUri } = require('./config/conf')
 const app = express()
+const recursive = require('recursive-readdir-sync');
 
 app.use(cors());
 app.options('*', cors());
 app.use(express.json())
-app.use(apiUri, router)
+
+recursive(`${__dirname}/task`).forEach((file) =>
+	app.use(apiUri, require(file))
+);
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`)
