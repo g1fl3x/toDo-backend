c85c778f2c0d45082922254288c81d692d47b124
g1fl3x <g1fl3x>
2022-02-03 16:08:17 +0300

    move router.js logic to app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const { appPort, apiUri } = require('./config/config')
+const recursive = require('recursive-readdir-sync');
 const app = express()
 
 app.use(cors());
 app.options('*', cors());
 app.use(express.json())
-app.use(apiUri, require('./task/router'))
+
+recursive(`${__dirname}/task`).forEach((file) => {
+    app.use(apiUri, require(file))
+});
 
 app.listen(appPort, () => {
 	console.log(`App listening on port ${appPort}`)
